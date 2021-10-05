b06b36eec01463fc26b9669571b4c35fcc421de5
taku8a <takuya0505hsmt@gmail.com>
2021-10-05 11:29:28 +0000

    search for

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,6 +12,19 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def self.search_for(content, method)
+    if method == "perfect"
+      Book.where(title: content)
+    elsif method == "forward"
+      Book.where("title LIKE?", content + "%")
+    elsif method == "backward"
+      Book.where("title LIKE?", "%" + content)
+    else
+      Book.where("title LIKE?", "%" + content + "%")
+    end
+  end
+
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
