cf0a03053d1a4c66a2a596468bbc6fe2bf0b5cc3
taku8a <takuya0505hsmt@gmail.com>
2021-10-08 02:57:08 +0000

    star1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_035349) do
+ActiveRecord::Schema.define(version: 2021_10_08_010935) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_10_06_035349) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate"
   end
 
   create_table "chats", force: :cascade do |t|
